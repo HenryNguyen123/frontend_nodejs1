8abf5a97a86ff7a3edfc88cfc38af133d86610fc
Minh Nhat <nhokkudo143@gmail.com>
2026-06-10 10:15:25 +0700

    push new source

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,10 +1,16 @@
-
+import './home.scss'
 
 const HomeComponent = () => {
     
     return (
         <>
-            <h1>Home</h1>
+            <div className='home'>
+                <div className='box1'></div>
+                <div className='box2'>
+                    <h1 className="main-h1">Mai cúp tập phạt 500k :))</h1>
+                </div>
+                <div className='box1'></div>
+            </div>
         </>
     )
 }
